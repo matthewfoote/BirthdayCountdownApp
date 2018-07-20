848a8479186e8a6092c6cfed60f4bea8f3794836
Matthew <matthewfoote1127@gmail.com>
2018-07-20 03:08:20 +0000

    fixed the Change Date Button's Angle

--- src/components/app.js
+++ src/components/app.js
@@ -37,9 +37,10 @@ export default class App extends Component {
     return (
       <div className="grid">
         <h1 className="grid__title">Birthday Countdown</h1>
-        <div className="grid__skew-dark-one-box"></div>
+
         <div className="grid__skew-dark-two"></div>
         <div className="grid__skew-dark-three"></div>
+        
         <div className="grid__skew-light-one"></div>
         <div className="grid__skew-light-two"></div>
         <div className="grid__skew-light-three-box"></div>
